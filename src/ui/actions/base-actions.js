c220963a0057a518994f64274a4eceef6381b82a
Bhavesh <bhavesh.gupta@indianic.com>
2018-12-04 17:39:00 +0530

    first face completed

--- src/ui/actions/base-actions.js
+++ src/ui/actions/base-actions.js
@@ -5,7 +5,7 @@ const apiUrl = config.serverUrl + "/api/common";
 // var baseObj = {
 //   model:'wigets',
 //   redux:'GET_WIDGETS',
-//   data:{
+//   item:{
 //     uid:""
 //   },
 //   uid:"",
@@ -13,107 +13,107 @@ const apiUrl = config.serverUrl + "/api/common";
 //   caseSensitive:false
 // }
 function getItems(baseObj) {
-  console.log(baseObj)
-  return async function (dispatch) {
-    axios({
-      method: "post",
-      url: apiUrl,
-      data: {
-        type: baseObj.redux || 'GET_WIDGETS',
-        options: baseObj
-      }
-    })
-      .then(function (response) {
-        // console.log(response.data)
-        if (response.success === false) {
-          return response;
-        }
-        return dispatch({
-          type: baseObj.redux || 'GET_WIDGETS',
-          payload: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    console.log(baseObj)
+    return async function(dispatch) {
+        axios({
+            method: "post",
+            url: apiUrl,
+            data: {
+                type: baseObj.redux || 'GET_WIDGETS',
+                options: baseObj
+            }
+        })
+            .then(function(response) {
+                // console.log(response.data)
+                if (response.success === false) {
+                    return response;
+                }
+                return dispatch({
+                    type: baseObj.redux || 'GET_WIDGETS',
+                    payload: response.data
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
 }
 function updateitem(baseObj) {
-  console.log('baseObj=', baseObj)
-  if (baseObj && baseObj.uid) {
-    return async function (dispatch) {
-      axios({
-        method: 'post',
-        url: apiUrl + "/update",
-        data: {
-          model: baseObj.model, //||'widgets',
-          options: baseObj
+    console.log('baseObj=', baseObj)
+    if (baseObj && baseObj.uid) {
+        return async function(dispatch) {
+            axios({
+                method: 'post',
+                url: apiUrl + "/update",
+                data: {
+                    model: baseObj.model, //||'widgets',
+                    options: baseObj
+                }
+            })
+                .then(function(response) {
+                    console.log(response.data)
+                    return dispatch({
+                        type: baseObj.redux || 'UPDATE_WIDGETS',
+                        payload: response.data,
+                        options: baseObj
+                    });
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
         }
-      })
-        .then(function (response) {
-          console.log(response.data)
-          return dispatch({
-            type: baseObj.redux || 'UPDATE_WIDGETS',
-            payload: response.data,
-            options: baseObj
-          });
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
     }
-  }
 
 }
 
 function createItem(baseObj) {
-  console.log('data=', baseObj)
-  if (baseObj) {
-    return async function (dispatch) {
-      axios({
-        method: 'post',
-        url: apiUrl + "/add",
-        data: {
-          model: baseObj.model, //|| 'widgets'
-          options: baseObj
+    console.log('data=', baseObj)
+    if (baseObj) {
+        return async function(dispatch) {
+            axios({
+                method: 'post',
+                url: apiUrl + "/add",
+                data: {
+                    model: baseObj.model, //|| 'widgets'
+                    options: baseObj
+                }
+            })
+                .then(function(response) {
+                    console.log(response.data)
+                    return dispatch({
+                        type: baseObj.redux || 'UPDATE_WIDGETS',
+                        payload: response.data
+                    });
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
         }
-      })
-        .then(function (response) {
-          console.log(response.data)
-          return dispatch({
-            type: baseObj.redux || 'UPDATE_WIDGETS',
-            payload: response.data
-          });
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
     }
-  }
 }
 
 function deleteItem(baseObj) {
-  return async function (dispatch) {
-    axios({
-      method: 'post',
-      url: apiUrl + "/delete",
-      data: {
-        uid: baseObj.uid,
-        model: baseObj.model, //|| 'widgets'
-        options: baseObj
-      }
-    })
-      .then(function (response) {
-        console.log(response.data)
-        return dispatch({
-          type: 'REMOVE_WIDGETS',
-          payload: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    return async function(dispatch) {
+        axios({
+            method: 'post',
+            url: apiUrl + "/delete",
+            data: {
+                uid: baseObj.uid,
+                model: baseObj.model, //|| 'widgets'
+                options: baseObj
+            }
+        })
+            .then(function(response) {
+                console.log(response.data)
+                return dispatch({
+                    type: baseObj.redux || 'REMOVE_WIDGETS',
+                    payload: response.data
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
 }
 // function searchWidgets(baseObj) {
 //   if (searchText) {
@@ -141,9 +141,9 @@ function deleteItem(baseObj) {
 
 // }
 export default {
-  createItem,
-  getItems,
-  deleteItem,
-  updateitem
-  // searchWidgets
+    createItem,
+    getItems,
+    deleteItem,
+    updateitem
+    // searchWidgets
 }
